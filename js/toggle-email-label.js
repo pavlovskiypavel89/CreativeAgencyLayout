d3da3300ad4ee575bc60c3b33fac9036d02c9fd8
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-10-25 11:23:10 +0700

    Add files via upload

---
+++ js/toggle-email-label.js
@@ -0,0 +1,4 @@
+﻿"use strict";
+const input = document.getElementById("email");
+const [label] = document.getElementsByClassName("email-label");
+input.addEventListener("blur", event => label.classList.toggle("hidden", event.currentTarget.value));
