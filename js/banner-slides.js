5ed8ed74bd689f368310649aaaf9f1276abf061d
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-10-26 21:33:29 +0700

    Update banner-slides.js

--- js/banner-slides.js
+++ js/banner-slides.js
@@ -1,20 +1,20 @@
-﻿"use strict";
-const [banner] = document.getElementsByClassName('banner-section');
-const [slides] = banner.getElementsByClassName('banner-slides');
-const [slider] = banner.getElementsByClassName('banner-slider');
+"use strict";
+const [banner] = document.getElementsByClassName("banner-section");
+const [slides] = banner.getElementsByClassName("banner-slides");
+const [slider] = banner.getElementsByClassName("banner-slider");
 
 const toggleSlider = activatedBtn => {
-  slider.querySelector('.active').classList.remove('active');
-  activatedBtn.classList.add('active');
+  slider.querySelector(".active").classList.remove("active");
+  activatedBtn.classList.add("active");
 }
 
 const changeSlide = sliderBtns => {
   const activatedSlideNum = Array.from(sliderBtns).findIndex(
-    btn => btn.classList.contains('active')
+    btn => btn.classList.contains("active")
   );
   
-  slides.querySelector('.current-slide').classList.remove('current-slide');
-  slides.getElementsByClassName('banner-slide')[activatedSlideNum].classList.add('current-slide');
+  slides.querySelector(".current-slide").classList.remove("current-slide");
+  slides.getElementsByClassName("banner-slide")[activatedSlideNum].classList.add("current-slide");
 }
 
 const flipSlide = event => {
@@ -24,4 +24,4 @@ const flipSlide = event => {
   }
 }
 
-slider.addEventListener('click', flipSlide);
+slider.addEventListener("click", flipSlide);
